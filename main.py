c2ec805e72906bf93e2b472bb006d732e3b7c06d
ttoth2 <118882072+ttoth2@users.noreply.github.com>
2023-04-19 07:20:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 
-class Square:
+class Négyzet:
     def __init__(self, x, y, width, height):
         self.x = x
         self.y = y
@@ -8,13 +8,11 @@ class Square:
         self.height = height
         self.abs_x = x * width
         self.abs_y = y * height
-        self.abs_pos = (self.abs_x, self.abs_y)
         self.pos = (x, y)
         self.color = 'light' if (x + y) % 2 == 0 else 'dark'
         self.draw_color = (220, 208, 194) if self.color == 'light' else (53, 53, 53)
         self.highlight_color = (100, 249, 83) if self.color == 'light' else (0, 228, 10)
         self.occupying_piece = None
-        self.coord = self.get_coord()
         self.highlight = False
         self.rect = pygame.Rect(
             self.abs_x,
@@ -22,8 +20,43 @@ class Square:
             self.width,
             self.height
         )
+class Board:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.tile_width = width // 8
+        self.tile_height = height // 8
+        self.selected_piece = None
+        self.turn = 'white'
+        self.config = [
+            ['bR', 'bN', 'bB', 'bQ', 'bK', 'bB', 'bN', 'bR'],
+            ['bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP'],
+            ['','','','','','','',''],
+            ['','','','','','','',''],
+            ['','','','','','','',''],
+            ['','','','','','','',''],
+            ['wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP'],
+            ['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR'],
+        ]
+        self.squares = self.generate_squares()
+        self.setup_board()
 
+ def generate_squares(self):
+        output = []
+        for y in range(8):
+            for x in range(8):
+                output.append(
+                    Square(x,  y, self.tile_width, self.tile_height)
+                )
+        return output
 
+    def get_square_from_pos(self, pos):
+        for square in self.squares:
+            if (square.x, square.y) == (pos[0], pos[1]):
+                return square
+
+    def get_piece_from_pos(self, pos):
+        return self.get_square_from_pos(pos).occupying_piece
 
 
 
